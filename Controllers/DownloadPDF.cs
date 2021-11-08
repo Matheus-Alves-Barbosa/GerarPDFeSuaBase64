7aa93bda5b9c20d7ab5abdd55fa525f450408fbb
Matheus Alves Barbosa <t2m.mbarbosa@terceiros.mag.com.br>
2021-11-08 20:27:05 -0300

    Atualização

---
+++ Controllers/DownloadPDF.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerarPDFeBase64.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DownloadPDFController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult DonwloadPDF ()
+        {
+            return File(GerarPDF.GeraPDF(), System.Net.Mime.MediaTypeNames.Application.Octet, "arquivoTeste-" + Guid.NewGuid().ToString() + ".pdf");
+        }
+    }
+}
---
+++ Controllers/GerarPDFBase64.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace GerarPDFeBase64.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GerarPDFBase64Controller : ControllerBase
+    {
+        [HttpGet("{qtd}")]
+        public List<string> GeraPDFBase64(int qtd)
+        {
+            List<string> lbase64 = new List<string>();
+            for (int i = 1; i <= qtd; i++)
+            {
+                lbase64.Add(Convert.ToBase64String(GerarPDF.GeraPDF()));
+            }
+            return lbase64;
+        }
+    }
+}
